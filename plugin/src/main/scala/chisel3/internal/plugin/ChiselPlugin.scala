22edbf8d8544b7c11daa1cb96243d3bd6b49a7ba
chick <chick.markley@sifive.com>
2021-12-30 14:02:09 -0800

    Turned elements processing back on

--- plugin/src/main/scala/chisel3/internal/plugin/ChiselPlugin.scala
+++ plugin/src/main/scala/chisel3/internal/plugin/ChiselPlugin.scala
@@ -10,7 +10,7 @@ import scala.collection.mutable
 
 private[plugin] case class ChiselPluginArguments(
   val skipFiles: mutable.HashSet[String] = mutable.HashSet.empty,
-  var buildElementsAccessor: Boolean = false,
+  var buildElementsAccessor: Boolean = true,
   var pluginDebugBundlePattern: String = "" // For example try .*Bpip.*"
 ) {
   def useBundlePluginOpt = "useBundlePlugin"
